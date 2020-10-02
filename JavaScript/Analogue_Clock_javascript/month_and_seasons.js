cd575f3f0b77d19be1553455468b68adaec5a8d4
allegium <71918162+allegium@users.noreply.github.com>
2020-10-02 13:41:02 +0300

    Create month_and_seasons.js

---
+++ JavaScript/Analogue_Clock_javascript/month_and_seasons.js
@@ -0,0 +1,13 @@
+const month = 11
+if (month>=1 && month <=3) {
+console.log ( "Winter")
+}
+if (month>=4 && month<=6) {
+console.log ("Spring")
+}
+if (month>=7 && month<=9) {
+console.log ("Summer")
+}
+if (month>=10 && month<=12) {
+console.log ("Autumm")
+}
