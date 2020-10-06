b37407d9dcd94c3d38c467427718103ad8167256
Sourabh Kumar Singh <sourabh6sept@gmail.com>
2020-10-06 20:15:43 +0530

    add file via upload
    Simple dice simulator

---
+++ Python/dicesimulator.py
@@ -0,0 +1,45 @@
+import random
+
+x = 'y'
+print('This is a dice simulator')
+while x=='y':
+	number = random.randint(1,6)
+
+	if number == 1:
+		print("----------")
+		print("|        |")
+		print("|    0   |")
+		print("|        |")
+		print("----------")	
+	if number == 2:
+		print("----------")
+		print("|        |")
+		print("| 0    0 |")
+		print("|        |")
+		print("----------")	
+	if number == 3:
+		print("----------")
+		print("|    0   |")
+		print("|    0   |")
+		print("|    0   |")
+		print("----------")	
+	if number == 4:
+		print("----------")
+		print("| 0    0 |")
+		print("|        |")
+		print("| 0    0 |")
+		print("----------")	
+	if number == 5:
+		print("----------")
+		print("| 0    0 |")
+		print("|    0   |")
+		print("| 0    0 |")
+		print("----------")		
+	if number == 6:
+		print("----------")
+		print("| 0    0 |")
+		print("| 0    0 |")
+		print("| 0    0 |")
+		print("----------")	
+
+	x = input('Press y to roll again ')
