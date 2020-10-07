320c8618f1b3fef13595bfcdeb6ff892b033f267
Harsh Balaprasad Baheti <65105435+BAHETIHARSH@users.noreply.github.com>
2020-10-07 16:10:48 +0530

    Update Factorial.py

--- Python/Factorial.py
+++ Python/Factorial.py
@@ -7,4 +7,4 @@ elif num == 0:
 else:
    for i in range(1,num + 1):
        factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+   print("The factorial of {} is {}".format(num,factorial))
