961338be3f1be1852795c57bea52b9c12ace46d0
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-09 23:22:41 +0530

    Create BST_height.java

---
+++ Java/BST_height.java
@@ -0,0 +1,49 @@
+//code to calculate height of a BST
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class BST_height {
+	public static int getHeight(Node root){//recursive function to count height by recursivly changing the node
+      if(root == null) {
+           return -1;
+       }      
+       int left = 1 + getHeight(root.left);
+       int right = 1 + getHeight(root.right);        
+       return Math.max(left, right);
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+	 public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
