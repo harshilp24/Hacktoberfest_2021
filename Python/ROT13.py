892a7e8583eb608ac611471de6468d4b21d1f4a7
GantaChaitra <gantachaitra@gmail.com>
2020-10-22 18:48:39 +0530

    HacktoberFest : Python program for ROT13 .

---
+++ Python/ROT13.py
@@ -0,0 +1,19 @@
+#ROT13 is a simple letter substitution cipher that replaces a letter with the 13th letter after it in the alphabet.
+
+def rot13(phrase):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    out_phrase = ""
+    for char in phrase:
+        out_phrase += abc[(abc.find(char)+13)%26]
+    return out_phrase
+
+x = "xthexrussiansxarexcoming"
+print(rot13(x))
+
+
+print(rot13(rot13(x)))
+
+#Output :kgurkehffvnafknerkpbzvat
+
+#        xthexrussiansxarexcoming
+
