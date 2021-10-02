e354fafeac9abccc0c898f98a1fb84253ac202f4
Soumya Das <get.soumya2000@gmail.com>
2021-10-02 23:25:03 +0530

    created dynamic quiz app

---
+++ Quiz-App/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "As we all know Neeraj Chopra won Gold at Tokyo Olympics in men’s Javelin. So, what was the max distance thrown by him.",
+    answer: "87.58m",
+    options: [
+      "87.56m",
+      "87.85m",
+      "87.58m",
+      "87.95m"
+    ]
+  },
+    {
+    numb: 2,
+    question: "At the end of Indo-Pak war of 1971, two nations agreed to rename the ceasefire line as?",
+    answer: "Line of Control",
+    options: [
+      "de facto border",
+      "Line of Control",
+      "Pok",
+      "No man’s land"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name of the Award for Top Run Scorer in the ICC World Cup?",
+    answer: "Golden Bat Award ",
+    options: [
+      "Golden Bat Award ",
+      "Golden Ball Award",
+      "Golden Runner Award",
+      "Golden Wicket Award"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What were Messi’s two Barcelona squad numbers before No. 10?",
+    answer: "30 & 19",
+    options: [
+      "29 & 17",
+      "11 & 20",
+      "30 & 11",
+      "30 & 19"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Indian IT industry consist second highest largest number of employees?",
+    answer: "Infosys",
+    options: [
+      "TCS",
+      "Wirpo",
+      "HCL Technologies",
+      "Infosys"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Captain Vikram Batra aka Shershaah was granted for which award?",
+    answer: "Param Vir Chakra",
+    options: [
+      "Shaurya Chakra",
+      "Param Vir Chakra",
+      "Ashoka Chakra",
+      "Param Vishisht Seva "
+    ]
+  },
+];
