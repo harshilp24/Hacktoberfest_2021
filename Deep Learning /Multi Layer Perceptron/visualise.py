cf68b24612df81c6e7f583e5815dd307f50dd007
parth sharma <parth.sharma199942@gmail.com>
2021-10-05 11:45:09 +0530

    Deep learning algorithms MLP and a single neuron written from scratch

---
+++ Deep Learning /Multi Layer Perceptron/visualise.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def plot_decision_boundary(model, X, y,cmap=plt.cm.jet):
+    """
+    Given a model(a function) and a set of points(X), corresponding labels(y), scatter the points in X with color coding
+    according to y. Also use the model to predict the label at grid points to get the region for each label, and thus the 
+    descion boundary.
+    Example usage:
+    say we have a function predict(x,other params) which makes 0/1 prediction for point x and we want to plot
+    train set then call as:
+    plot_decision_boundary(lambda x:predict(x,other params),X_train,Y_train)
+    params(3): 
+        model : a function which expectes the point to make 0/1 label prediction
+        X : a (mx2) numpy array with the points
+        y : a (mx1) numpy array with labels
+    outputs(None)
+    """
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.style.use("seaborn")
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.jet)
+    
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
